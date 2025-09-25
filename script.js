23e24f10fa5614eb35f07e17cde32eb6f3fc35f8
aboulayetchakoura <aboulayet63@gmail.com>
2025-09-25 12:11:51 -0400

    get data and display them on the screen

--- script.js
+++ script.js
@@ -1,23 +1,110 @@
  const displayInput= document.querySelector(".display-input-field")
- const  inputDelete = document.querySelector(".display-input-field")
- const fasSearch = document.querySelector(".fas")
+ const  deleteBtn = document.querySelector("#delete-btn")
+ const fasSearch = document.querySelector("#search-icon")
  const inputValue = document.querySelector('.input-value')
+ const submit = document.querySelector("#submit-form")
+ const infoText = document.querySelector(".info-text")
+ const resultContent = document.querySelector(".result-content")
 
-function showInputField(){
-  fasSearch.style.display = 'none'    
-  const deleteBtn = document.querySelector('.delete')
-  displayInput.style.display='block'
-  deleteBtn.addEventListener("click", ()=>{
+
+// // Show input when search icon is clicked
+function showInputField() {
+  console.log(displayInput)
+  fasSearch.classList.add('hide');
+  setTimeout(() => {
+    fasSearch.classList.add('hidden')
+    displayInput.classList.remove("hidden")
+    requestAnimationFrame(()=>{
+      displayInput.classList.add("show")
+    })
+  }, 300);
+}
+
+// // Hide input when delete button is clicked
+deleteBtn.addEventListener('click', () => {
+     inputValue.value =''
+     displayInput.classList.remove('show')
+     setTimeout(() => {
+       displayInput.classList.add("hidden")
+       fasSearch.classList.remove('hidden')
+       requestAnimationFrame(()=>{
+        fasSearch.classList.remove('hide')
+       })
+     }, 300);
+});
+
+
+const fetchArticles = async ()=>{
+  console.log('val',inputValue.value)
+  let min = 0;
+  let max = 10
+const randomRange = (Math.random() + 1 ) ;
+console.log('random', randomRange)
+  const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=&gsrlimit=20&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${inputValue.value}&origin=*`
+  //`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=javascript&format=json&origin=*`
+  const response = await fetch(url)
+  
+  const data = await response.json()
+  console.log('length',data)
+  
+  const queries = data.query;
+  const pages = queries.pages
+ 
+  const pagesKeys = Object.keys(pages)
+  let  responseContent =``
+   pagesKeys.forEach(key => {
+     console.log('ke', key)
+       console.log("pages", pages)
+   // console.log(pages[key].thumbnail)
     
-      inputDelete.style.display='none'
-      fasSearch.style.display = 'block'
-  })
+
+
+     if( pages[key].extract&&pages[key].thumbnail){
+      //console.log(pages[key].thumbnail.source)
+      const title = pages[key].title;
+      
+      
+      
+     const extract = pages[key].extract
+     
+   
+
+       responseContent += `<a href="https://en.wikipedia.org/?curid=${key}" target="_blank">
+         <li class='list'>
+          <h1 class='title'>${title}</h1>
+          <p class='extract'>${extract}</p>
+         </li>
+       </a>`
+
+
+      }else{
+       return;
+      }
+     
+       
+   
+ 
+       
+   }
+  
+  )
+  
+// console.log('res cont', resultContent)
+ resultContent.innerHTML = responseContent
+
 }
 
 
+ inputValue.addEventListener("keydown", (event)=>{
+   if(event.key ==="Enter"){
+     fetchArticles()
+     inputValue.value =''
+     event.preventDefault()
+ }
 
-// since we used type module as script.js attribute, 
-//we need to call the function on window so the onclick event
-//in html knows the function really exist else it will throu 
-// a function not difined error;
-window.showInputField = showInputField
+ })
+
+
+ window.showInputField = showInputField
+
+
